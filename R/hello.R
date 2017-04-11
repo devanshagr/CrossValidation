40f5bae451c65e1f3f9be5f1637e14e4e6437e24
neelesh314 <neelesh.kumar@rutgers.edu>
2017-04-10 22:35:23 -0400

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,30 +1,35 @@
-cross_validate<-function(df,dep,N,ss,sr)
+cross_validate1<-function(df,dep,n_indep,n_iter,sr)
 {
   library("caTools")
   library("rpart")
-  set.seed(ss)
-  spl <- sample.split(df[,dep], SplitRatio = sr)
-  #Randomly splits into two subsets 0.7 and 0.3 of the original file
-  #into training and testing
-  train <- subset(df, spl == TRUE)
-  testing <- subset(df, spl ==FALSE)
   #We will compare two models- predictor
   indep=list()
   print("Enter the independent variables")
   i<-1
-  while(i<=N){
+  while(i<=n_indep){
     str=readline()
     indep[[i]]<-str
     i<-i+1
   }
   b <- paste(indep, collapse ="+")
-  first.tree<-rpart(as.formula(paste(dep,b,sep="~")),data=train)
-  second.tree<-rpart(as.formula(paste(dep,'.',sep="~")), data=train)
+  mean_subset<-c();
+  mean_all<-c();
+  relation_subset=as.formula(paste(dep,b,sep="~"))
+  relation_all=as.formula(paste(dep,'.',sep="~"))
+  for(i in 1:n_iter){
+  spl <- sample.split(df[,dep], SplitRatio = sr)
+  #Randomly splits into two subsets 0.7 and 0.3 of the original file
+  #into training and testing
+  train <- subset(df, spl == TRUE)
+  testing <- subset(df, spl ==FALSE)
+  first.tree<-rpart(relation_subset,data=train)
+  second.tree<-rpart(relation_all, data=train)
   pred1.tree<-predict(first.tree,newdata=testing, type='class')
   pred2.tree<-predict(second.tree, newdata=testing, type='class')
   mean1<-mean(pred1.tree==testing[,dep])
   mean2<-mean(pred2.tree==testing[,dep])
-
-  return(c(mean1,mean2))
-
+  mean_subset<-c(mean_subset,mean1);
+  mean_all<-c(mean_all,mean2);
+  } 
+  return (c(mean(mean_subset),mean(mean_all)))
 }
