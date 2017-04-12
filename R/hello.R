0594465791e69f2fa3467d618ee8e119e83e5e35
neelesh314 <neelesh.kumar@rutgers.edu>
2017-04-11 23:10:59 -0400

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -8,16 +8,14 @@ cross_validate<-function(df,tree,n_iter,sr)
   dep<-all.vars(terms(tree))[1]
   relation_all=as.formula(paste(dep,'.',sep="~"))
   for(i in 1:n_iter){
-  spl <- sample.split(df[,dep], SplitRatio = sr)
-  #Randomly splits into two subsets 0.7 and 0.3 of the original file
-  #into training and testing
-  train <- subset(df, spl == TRUE)
-  testing <- subset(df, spl ==FALSE)
+  sample <- sample.int(n = nrow(df), size = floor(sr*nrow(df)), replace = F)
+  train <- df[sample, ]
+  testing  <- df[-sample, ]
   second.tree<-rpart(relation_all, data=train)
   pred1.tree<-predict(tree,newdata=testing, type='class')
   pred2.tree<-predict(second.tree, newdata=testing, type='class')
-  mean1<-mean(pred1.tree==testing[,dep])
-  mean2<-mean(pred2.tree==testing[,dep])
+  mean1<-mean(as.character(pred1.tree)==testing[,dep])
+  mean2<-mean(as.character(pred2.tree)==testing[,dep])
   mean_subset<-c(mean_subset,mean1)
   mean_all<-c(mean_all,mean2)
   } 
